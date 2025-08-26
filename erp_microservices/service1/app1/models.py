3f0aa02a0f10caf0ee6244cf2c6a0a496a2bc978
Полевщиков Закария <140149054+HELLPUSYY666@users.noreply.github.com>
2025-08-26 21:23:26 +0500

    Update models.py

--- erp_microservices/service1/app1/models.py
+++ erp_microservices/service1/app1/models.py
@@ -2,10 +2,6 @@ from django.db import models
 
 
 class User(models.Model):
-    """
-    Пример модели для пользователей в ERP.
-    """
-
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
